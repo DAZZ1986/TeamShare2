6ebc9e85408ab441ce4279cc0f8a7678e7cc2f31
DAZZ1986 <50179402+DAZZ1986@users.noreply.github.com>
2021-07-29 15:15:28 +0300

    Add project files.

---
+++ TeamShare2/TeamShare2/CodeOnlyPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TeamShare2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CodeOnlyPage : ContentPage
+    {
+        public CodeOnlyPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            StackLayout layout = new StackLayout();
+
+            Label label1 = new Label();
+
+            label1.Text = "Urok 4";
+            label1.TextColor = Color.Cyan;
+            label1.FontSize = 20;
+            label1.FontAttributes = FontAttributes.Bold;
+
+            layout.Children.Add(label1);
+
+            Content = layout;
+        }
+
+    }
+}
---
+++ TeamShare2/TeamShare2/FirstPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TeamShare2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FirstPage : ContentPage
+    {
+        public FirstPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ButtonHello_Click(object sender, EventArgs e) 
+        {
+            Label1.Text = Entry1.Text;
+        }
+
+        private void ButtonHello_Click2(object send, EventArgs e)
+        {
+            Label2.Text = Entry2.Text;
+        }
+
+    }
+}
